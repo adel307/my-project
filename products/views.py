8959015f900195bd6d7dba07921b0c7e80867eb0
adel307 <adel.307.ahmed@gmail.com>
2025-09-16 18:16:03 +0300

    new updates

--- products/views.py
+++ products/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from pages.models import Product, Clint, Market
 
-# Create your views here.
-def Product(request):
-    return render(request,'products/products.html')
+def product_list(request):
+    """
+    عرض قائمة جميع المنتجات، العملاء، والأسواق
+    """
+
+    # جلب جميع البيانات مع تحسين الأداء
+    products = Product.objects.all()
+    clints = Clint.objects.all().select_related()
+    markets = Market.objects.all().prefetch_related('vesetors', 'market_products')
     
-
+    context = {
+        'products_num': str(products.count()),
+        'product_card': products.select_related('product_time'),
+        'clints_card': clints,
+        'markets_card': markets
+    }
+    
+    return render(request, 'products/products.html', context)
