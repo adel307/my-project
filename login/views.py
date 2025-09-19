8ca41eb6e6e4b7254d17ed4ca2d099d991e53917
adel307 <adel.307.ahmed@gmail.com>
2025-09-19 21:45:04 +0300

    new updates

--- login/views.py
+++ login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def login(request):
+    x = request.POST.get('user_name')
+    print(x)
     return render(request,'login/login.html',{'name':'adel101'})
