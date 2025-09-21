8423ae95f82e80eb9a75a8728067352d97aebf43
adel307 <adel.307.ahmed@gmail.com>
2025-09-21 16:30:55 +0300

    new updates

--- pages/admin.py
+++ pages/admin.py
@@ -4,6 +4,7 @@ from .models import Time , Clint , Product , Market
 class admin_Products(admin.ModelAdmin):
     list_display = ['id','name','price','cotegory','active','product_time']
     list_editable = ['name','price','cotegory','active','product_time']
+    list_display_links = ['id']
 
 class admin_Time(admin.ModelAdmin):
     list_display = ['Date','time','moment']
@@ -13,11 +14,17 @@ class admin_Time(admin.ModelAdmin):
 class admin_Clint(admin.ModelAdmin):
     list_display = ['id','name','password','phone']
     list_editable = ['name','password','phone']
+    list_display_links = ['id']
+
+class admin_Market(admin.ModelAdmin):
+    list_display = ['id','name','place']
+    list_editable = ['name','place']
+    list_display_links = ['id']
     
 admin.site.register(Product , admin_Products)
 admin.site.register(Time , admin_Time)
 admin.site.register(Clint , admin_Clint)
-admin.site.register(Market)
+admin.site.register(Market,admin_Market)
 
 admin.site.site_header = 'wed site management'
 admin.site.site_title = 'products management'
